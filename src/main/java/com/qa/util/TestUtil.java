d997ab1f01cb7a3c21d299feeb3d61fbde9683ee
Pratham <pratham.soni10@gmail.com>
2019-09-14 19:51:38 +0530

    Commit for Practice purpose

--- src/main/java/com/qa/util/TestUtil.java
+++ src/main/java/com/qa/util/TestUtil.java
@@ -4,6 +4,6 @@ public class TestUtil {
 
     public void testUtil(){
         System.out.println("Test Util");
-        System.out.println("Pratham added Test Util again");
+
     }
 }
--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -4,6 +4,7 @@ public class LoginTest {
 
     public void loginTest() {
         System.out.println("Test class");
+        System.out.println("For the sake of commit check");
 
     }
 }
